c26edce5238d56b47cabc72d428e3de09f504f66
billy8407 <billy8407@gmail.com>
2021-08-20 00:15:49 +0800

    Add customize logging

---
+++ utility/function_wrapper.py
@@ -0,0 +1,25 @@
+from functools import wraps
+import time
+import traceback
+from utility.log_handler import logger
+
+test_dict = {
+    '1': 'This is a warning',
+    '2': 'This is an error'
+}
+
+
+def log_measure(func):
+    log_dict = {'orignal_func': func.__name__}
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            logger.warning(test_dict['1'], extra=log_dict)
+            return result
+        except:
+            # string traceback
+            log_dict['traceback'] = traceback.format_exc()
+            logger.error(test_dict['2'], extra=log_dict)
+    return wrap
